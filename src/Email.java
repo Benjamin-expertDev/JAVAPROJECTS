d7194a00033b2d35fb399fa227d736f86a1e14ae
Benjamin-expertDev <benjaminmwambura@gmail.com>
2024-09-24 20:30:34 +0300

    first commit

---
+++ src/Email.java
@@ -0,0 +1,24 @@
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to recieve the first name and lat name
+    public Email(String firsrtName, String lastName){
+        this.firstName = firsrtName;
+        this.lastName = lastName;
+        System.out.println("Email created" + "" + this.firstName + "" + this.lastName);
+    }
+    //Ask for the department
+
+    //Generate a random password
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
+}
