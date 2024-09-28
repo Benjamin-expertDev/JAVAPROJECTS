85679a1c85bd1c5211267939e01ae940725b19b3
Benjamin-expertDev <benjaminmwambura@gmail.com>
2024-09-28 13:10:36 +0300

    commit

--- src/EmailApp.java
+++ src/EmailApp.java
@@ -1,7 +1,7 @@
 public class EmailApp {
 
     public static void main(String[] args) {
-        Email em1 = new Email("John", "Kamau")
+        Email em1 = new Email("John", "Kamau");
     }
     
 }
